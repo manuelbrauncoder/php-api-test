68c9da2226735a136c19b7a2211133c90de31df2
Manuel Braun <manuel.braun.coder@gmail.com>
2024-11-21 19:35:27 +0100

    Initial Commit

---
+++ cruises/cruisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  cruisesApp.swift
+//  cruises
+//
+//  Created by Manuel Braun on 21.11.24.
+//
+
+import SwiftUI
+
+@main
+struct cruisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
